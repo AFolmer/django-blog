a019e7f53283927d6f6b4cbb4ccd2bf418e6eb39
CaseNightmareGreen <132308533+CaseNightmareGreen@users.noreply.github.com>
2024-03-12 10:56:53 -0700

    Test created and passed for blogging

--- mysite/blogging/tests.py
+++ mysite/blogging/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+# Tests for blogging app
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+
+
+class PostTestCase(TestCase):
+    fixtures = ['blogging_test_fixture.json',]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+        print(p1)
+        print(actual)
