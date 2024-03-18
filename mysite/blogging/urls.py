59f829f9f430219d7041ecc9ce095f9f8e75d1b4
CaseNightmareGreen <132308533+CaseNightmareGreen@users.noreply.github.com>
2024-03-18 14:47:23 -0700

    Blogging detail view updated to match exercise

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -20,6 +20,6 @@ from .views import list_view, detail_view, stub_view
 
 urlpatterns = [
     path('posts', list_view, name="post_list"),
-    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
+    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
     path('', list_view, name="blog_index")
 ]
