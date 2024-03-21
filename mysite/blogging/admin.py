784c9bb984b5488a88e86f10a02f5c08404b9b6b
CaseNightmareGreen <132308533+CaseNightmareGreen@users.noreply.github.com>
2024-03-21 10:15:16 -0700

    First admin update with Admin classes works

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -3,5 +3,17 @@ from .models import Post, Category
 
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
