334d3e696b2eddef5f36f3b9d24602d4af4a7363
CaseNightmareGreen <132308533+CaseNightmareGreen@users.noreply.github.com>
2024-04-22 16:29:58 -0700

    Polling app modified to use class based views

--- mysite/polling/urls.py
+++ mysite/polling/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import list_view, detail_view
+from .views import PollListView, PollDetailView
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
+    path('', PollListView.as_view(), name="poll_index"),
+    path('polls/<int:pk>/', PollDetailView.as_view(), name="poll_detail"),
 ]
